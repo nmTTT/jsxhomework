186ada78e61baabec5171ab058c281024103d70b
Nomt Tumurkhuu <nmt@nmT-2.local>
2024-07-19 10:18:43 +0800

    jsx homework done

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Header from "@/component/header";
+import Hero from "@/component/hero";
+import Testimonial from "@/component/testimonial";
+import Footer from "@/component/footer";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Testimonial />
+    </>
+  );
+}
